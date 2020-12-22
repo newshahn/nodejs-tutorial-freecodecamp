7fc14e895073a78514d65d4636cb26f0761d88bf
Newsha Nik <nioosha.h@gmail.com>
2020-12-22 22:20:32 +0330

    working with nodejs modules

--- tutorial.js
+++ tutorial.js
@@ -1,2 +1,11 @@
 const sum = (num1, num2) => num1 + num2;
-module.exports = sum;
+const PI = 3.14;
+// const someMathObject = {
+//   constructor() {
+//     console.log("object created");
+//   },
+// };
+
+module.exports.sum = sum;
+module.exports.PI = PI;
+// module.exports.someMathObject = someMathObject;
