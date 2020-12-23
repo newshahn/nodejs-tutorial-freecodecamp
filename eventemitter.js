a5b364ab4358cb0452380672df78fb665fdf060b
newsha <nioosha.h@gmail.com>
2020-12-23 18:05:05 +0330

    extended eventemitter class and created a person class

--- eventemitter.js
+++ eventemitter.js
@@ -6,3 +6,22 @@ eventEmitter.on("tutorial", (num1, num2) => {
 });
 
 eventEmitter.emit("tutorial", 1, 2);
+
+class Person extends EventEmitter {
+  constructor(name) {
+    super();
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+}
+
+let Pedro = Person("Pedro");
+
+Pedro.on("name", () => {
+  console.log("my name is" + Pedro.name);
+});
+
+Pedro.emit("name");
