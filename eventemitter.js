b70e781799ab9663c1b85e8764e668db25abcde3
newsha <nioosha.h@gmail.com>
2020-12-23 17:48:17 +0330

    used eventemitter

---
+++ eventemitter.js
@@ -0,0 +1,8 @@
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on("tutorial", () => {
+  console.log("tutorial event has occured");
+});
+
+eventEmitter.emit("tutorial");
