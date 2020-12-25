c310f7ef49938c87ba0c245fb2a6eb558663e2a4
Newsha Nik <nioosha.h@gmail.com>
2020-12-25 21:29:26 +0330

    used fs.rename to rename example.txt to example2.txt

--- filesystemModule.js
+++ filesystemModule.js
@@ -1,17 +1,25 @@
 const fs = require("fs");
-// create a file
 
-fs.writeFile("example.txt", "this is an example", (err) => {
+// // create a file
+// fs.writeFile("example.txt", "this is an example", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("success");
+//     fs.readFile("example.txt", "utf8", (err, file) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log(file);
+//       }
+//     });
+//   }
+// });
+
+fs.rename("example.txt", "example2.txt", (err) => {
   if (err) {
     console.log(err);
   } else {
     console.log("success");
-    fs.readFile("example.txt", "utf8", (err, file) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(file);
-      }
-    });
   }
 });
