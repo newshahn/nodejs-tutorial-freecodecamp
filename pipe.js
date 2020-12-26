3e409554161f3dc11f32478b4a6f2d1a672b3791
newsha <nioosha.h@gmail.com>
2020-12-26 19:24:08 +0330

    used gunzip to decompress a file

--- pipe.js
+++ pipe.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const zlib = require("zlib");
-const gzip = zlib.createGzip();
-const readStream = fs.createReadStream("./example.txt", "utf8");
-const writeStream = fs.createWriteStream("example2.txt.gz");
-readStream.pipe(gzip).pipe(writeStream);
+const gunzip = zlib.createGunzip();
+const readStream = fs.createReadStream("./example2.txt.gz");
+const writeStream = fs.createWriteStream("uncompressed.txt");
+readStream.pipe(gunzip).pipe(writeStream);
