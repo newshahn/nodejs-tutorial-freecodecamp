c5405560a0b712a4a92e095cde745f24edf92dd2
newsha <nioosha.h@gmail.com>
2020-12-26 19:43:14 +0330

    used http module to build a server with node js

---
+++ httpModule.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("hello from node js");
+    res.end();
+  } else {
+    res.write("using some other domain");
+    res.end();
+  }
+});
+
+server.listen("3000");
