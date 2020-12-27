88b808ee8ae6329c6325fe73eace6f109268db30
newsha <nioosha.h@gmail.com>
2020-12-27 12:20:38 +0330

    learned when to use the query strings and when to use route parameteres

--- expressFramework.js
+++ expressFramework.js
@@ -11,7 +11,8 @@ app.get("/example", (req, res) => {
 
 app.get("/example/:name/:age", (req, res) => {
   console.log(req.params);
-  res.send("example with route params");
+  console.log(req.query);
+  res.send(req.params.name + ":" + req.params.age);
 });
 
 app.listen("3000");
