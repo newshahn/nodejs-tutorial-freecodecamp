3c797352a84b4029d34791a59cb2c301956af420
newsha <nioosha.h@gmail.com>
2020-12-27 12:49:11 +0330

    used path package and served static files using express

--- expressFramework.js
+++ expressFramework.js
@@ -1,18 +1,21 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+app.use("/public", express.static(path.join(__dirname, "static")));
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.sendFile(path.join(__dirname, "static", "style.css"));
 });
 
-app.get("/example", (req, res) => {
-  res.send("hitting example routes");
-});
+// app.get("/example", (req, res) => {
+//   res.send("hitting example routes");
+// });
 
-app.get("/example/:name/:age", (req, res) => {
-  console.log(req.params);
-  console.log(req.query);
-  res.send(req.params.name + ":" + req.params.age);
-});
+// app.get("/example/:name/:age", (req, res) => {
+//   console.log(req.params);
+//   console.log(req.query);
+//   res.send(req.params.name + ":" + req.params.age);
+// });
 
 app.listen("3000");
